4aef580543d5779325aae4c21e6994bba256ce11
ovoc01 <spotifymirindra@gmail.com>
2023-11-21 12:32:11 +0300

    creating tables for stats visualization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './css/bootstrap.css';
+import './css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container-fluid d-flex justify-content-center '>
+        <div className='row col-lg-8 mt-3'>
+          <div className='col-lg-8'>Season Stats</div>
+          <table class="table table-hover table table-bordered">
+            <thead className='table-dark'>
+              <tr>
+                <th scope="col">Classement</th>
+                <th scope="col">Joueur</th>
+                <th scope="col">MJ</th>
+                <th scope="col">PPM</th>
+                <th scope="col">RPM</th>
+                <th scope="col">PDPM</th>
+                <th scope="col">MPM</th>
+                <th scope="col">FG%</th>
+                <th scope="col">3P%</th>
+                <th scope="col">LF%</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Joel Embiid</td>
+                <td>13</td>
+                <td>31.9</td>
+                <td>11.2</td>
+                <td>6.2</td>
+                <td>34.1</td>
+                <td>55.2</td>
+                <td>31.8</td>
+                <td>88.5</td>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
 
